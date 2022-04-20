1cda0056298f497eacd1ccea95c2c513d4d11624
LuchiAdaobi <Luchichronicles@gmail.com>
2022-04-20 22:33:49 +0100

    added the dot functionality

--- main2.js
+++ main2.js
@@ -98,17 +98,3 @@ function findIndex(item, items){
           }
       }
 };
-
-
-// auto
-
-            var counter = 1;
-            setInterval(function(){
-                document.getElementById('radio' + counter).checked = true;
-                counter++;
-                if(counter > 6){
-                    counter = 1;
-                }
-            }, 5000);
-        
-            
